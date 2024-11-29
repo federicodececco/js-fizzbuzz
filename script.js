1e897cc4a4b14995a1ab0183e5a82ab04e897093
federicodececco <federicodececco1@gmail.com>
2024-11-29 12:57:48 +0100

    fix: logic

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        
+    if (i % 15 == 0) {
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 == 0) {
+
         console.log("fizz");
     }
     else if (i % 5 == 0) {
         console.log("buzz");
     }
-    else if(i%15==0){
+    else if (i % 15 == 0) {
         console.log("fizzBuzz")
     }
     else {
