df4ebe2d5a5ba34889602573506ba97999d1196a
federicodececco <federicodececco1@gmail.com>
2024-11-29 12:57:06 +0100

    fix: 15 missing

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
+        
         console.log("fizz");
     }
     else if (i % 5 == 0) {
         console.log("buzz");
+    }
+    else if(i%15==0){
+        console.log("fizzBuzz")
     }
     else {
         console.log(i);
